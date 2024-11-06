725dc8067c34cac0b1bac2d474470ca9cc6a9bd7
scarletcx <1753910738@qq.com>
2024-11-06 18:49:23 +0800

    更新3.7为前端版本

--- app/services/fishing_service.py
+++ app/services/fishing_service.py
@@ -69,7 +69,7 @@ def buy_bait(data):
     gmc_contract = ethereum_service.get_gmc_contract()
     user.user_gmc = gmc_contract.functions.balanceOf(user_id).call() * (10 ** -18)  # .call() 用于在本地执行合约函数，不会发起链上交易
     #更新数据库鱼饵数量user.user_baits += buy_amount
-    user.user_baits += buy_amount
+    user.user_baits = user.user_baits + buy_amount
     db.session.commit()
 
     return jsonify({
